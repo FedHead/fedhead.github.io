7b66ccc2a51dc40ab84b6dd8fb6caf693db5166b
FedHead <89026454+FedHead@users.noreply.github.com>
2025-01-09 13:28:21 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+
+const myImages = document.querySelectorAll("img");
+
+myImages.forEach((element) => element.addEventListener("click", () => {
+  const mySrc = element.getAttribute("src");
+  if (mySrc === "images/bonfire.png") {
+    element.setAttribute("src", "images/kappa.jpg");
+  } 
+  if (mySrc === "images/kappa.jpg") {
+    element.setAttribute("src", "images/bonfire.png");
+  }
+}));
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+        setUserName();
+      } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `カッパに興味がありますか, ${myName} ?`;
+  }
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `こんにちわ!, ${storedName}`;
+  }
+  
+
+myButton.addEventListener("click", () => {
+    setUserName();
+  });
+
